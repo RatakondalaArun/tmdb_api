13f0c31214e8bd99dc459162043f6cc76b9c45ba
Ratakondala Arun <arunratakondala3470@gmail.com>
2020-02-17 20:12:34 +0530

    removed testing

--- test/category/tv_episode_group_test.dart
+++ test/category/tv_episode_group_test.dart
@@ -1,23 +1,23 @@
-import 'package:flutter_test/flutter_test.dart';
-import '../../lib/keys.dart';
-import '../../lib/tmdb_api.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import '../../lib/keys.dart';
+// import '../../lib/tmdb_api.dart';
 
-main() {
-  TMDB tmdb = TMDB(Keys.API);
-  group('Tv Episode Group', () {
-    group('Details', () {
-      test('with out parameters', () async {
-        var result = await tmdb.tvEpisodeGroup.getDetails(103);
-        assert(result is Map);
-        expect(result.containsKey('status_code'), false);
-      });
-      test('with parameters', () async {
-        var result = await tmdb.tvEpisodeGroup.getDetails(103,
-            parameters:
-                Parameters(language: 'en-US', append: ['videos,images']));
-        assert(result is Map);
-        expect(result.containsKey('status_code'), false);
-      });
-    });
-  });
-}
+// main() {
+//   TMDB tmdb = TMDB(Keys.API);
+//   group('Tv Episode Group', () {
+//     group('Details', () {
+//       test('with out parameters', () async {
+//         var result = await tmdb.tvEpisodeGroup.getDetails(103);
+//         assert(result is Map);
+//         expect(result.containsKey('status_code'), false);
+//       });
+//       test('with parameters', () async {
+//         var result = await tmdb.tvEpisodeGroup.getDetails(103,
+//             parameters:
+//                 Parameters(language: 'en-US', append: ['videos,images']));
+//         assert(result is Map);
+//         expect(result.containsKey('status_code'), false);
+//       });
+//     });
+//   });
+// }
