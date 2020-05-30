9684682e0ed53034dff29a7db29294e6d90ec06b
Ratakondala Arun <arunratakondala3470@gmail.com>
2020-05-30 21:36:24 +0530

    reverted because CI tests failed because of missing file

--- test/init_script.dart
+++ test/init_script.dart
@@ -1,7 +1,8 @@
-import '../lib/keys.dart' as key;
 import 'dart:io' show Platform;
 
 class Keys {
-  static String API = Platform.environment['API_V3'] ?? key.Keys.API;
-  static String API_V4 = Platform.environment['API_V4'] ?? key.Keys.API_V4;
+  static String API =
+      Platform.environment['API_V3']; //added your API Keys for local test
+  static String API_V4 =
+      Platform.environment['API_V4']; //added your API Keys for local test
 }
