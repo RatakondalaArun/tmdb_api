4b4afc638de6dc03544448c1e2f88d4921c5469f
Ratakondala Arun <ratakondalaarun@gmail.com>
2021-10-05 12:56:34 +0530

    🚨 fixed lint errors

--- test/versions/v3/category/authentication_test.dart
+++ test/versions/v3/category/authentication_test.dart
@@ -12,7 +12,7 @@ void main() {
     group('Request token', () {
       test('As String', () async {
         final result = await tmdb.v3.auth.createRequestToken() as String;
-        expect(result == null, false);
+        expect(result, isA<String>());
       });
       test('As Map', () async {
         final result =
@@ -23,62 +23,72 @@ void main() {
 
     group('>createSession', () {
       test('>generating session id with username and password', () async {
-        String requestToken = await tmdb.v3.auth.createRequestToken() as String;
-        String? sessionId =
+        final requestToken = await tmdb.v3.auth.createRequestToken() as String;
+        final sessionId =
             await tmdb.v3.auth.createSession(requestToken) as String;
+        expect(sessionId, isA<String>());
       });
     });
     group('Create session with login', () {
       test('generate with creditionals', () async {
-        String? result = await tmdb.v3.auth.createSessionWithLogin(
-            user.User.USERNAME!, user.User.PASSWORD!) as String;
-        expect(result == null, false);
+        final result = await tmdb.v3.auth.createSessionWithLogin(
+          user.User.USERNAME!,
+          user.User.PASSWORD!,
+        ) as String?;
+        expect(result == null, isFalse);
       });
       test('>with wrong creditionals', () async {
-        String? result = await tmdb.v3.auth
-            .createSessionWithLogin('foobartest', 'foobarpass') as String;
-        expect(result == null, true);
+        final result = await tmdb.v3.auth
+            .createSessionWithLogin('foobartest', 'foobarpass') as String?;
+        expect(result == null, isTrue);
       });
       test('>with wrong creditionals as Map', () async {
-        Map? result = await tmdb.v3.auth
+        final result = await tmdb.v3.auth
                 .createSessionWithLogin('foobartest', 'foobarpass', asMap: true)
-            as Map;
-        expect(result is Map, true);
+            as Map?;
+        expect(result is Map, isTrue);
       });
     });
 
     group('>delete session', () {
       test('>with session id', () async {
-        final result = await (tmdb.v3.auth
-            .deleteSession('98097f2cd6af83f272ccbcfa93960723a940f87b'));
-        print(result);
+        final result = await tmdb.v3.auth.deleteSession(
+          '98097f2cd6af83f272ccbcfa93960723a940f87b',
+        );
         expect(result is Map, true);
       });
     });
 
     group('>guest session', () {
       test('>getting session as String', () async {
-        String? guestSessionId =
-            await tmdb.v3.auth.createGuestSession() as String;
+        final guestSessionId =
+            await tmdb.v3.auth.createGuestSession() as String?;
+        expect(guestSessionId, isNotNull);
       });
 
       test('>getting session as Map', () async {
-        Map? result = await tmdb.v3.auth.createGuestSession(asMap: true) as Map;
+        final result =
+            await tmdb.v3.auth.createGuestSession(asMap: true) as Map?;
+        expect(result, isNotNull);
       });
     });
 
     group('>session from v4 token', () {
       test('>getting session as String', () async {
-        final guestSessionId = await tmdb.v3.auth.createSessionFromV4AccessToken(
-                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE1ODQzNjgxODYsInN1YiI6IjVkZjEyMTYzZGI5NTJkMDAxOWJlZjAyNiIsImp0aSI6IjE5MDcxMDgiLCJhdWQiOiJjMjVkZDA2ODZmZDEyODk4YzJiMTg2ZTY5ZTY3MjhmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCIsImFwaV93cml0ZSJdLCJ2ZXJzaW9uIjoxfQ.cCgOIDlLk7F0LS-OlMJNl059TucIspxhhENuTW_dE1c')
-            as String;
+        final guestSessionId =
+            await tmdb.v3.auth.createSessionFromV4AccessToken(
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE1ODQzNjgxODYsInN1YiI6IjVkZjEyMTYzZGI5NTJkMDAxOWJlZjAyNiIsImp0aSI6IjE5MDcxMDgiLCJhdWQiOiJjMjVkZDA2ODZmZDEyODk4YzJiMTg2ZTY5ZTY3MjhmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCIsImFwaV93cml0ZSJdLCJ2ZXJzaW9uIjoxfQ.cCgOIDlLk7F0LS-OlMJNl059TucIspxhhENuTW_dE1c',
+        ) as String;
         print(guestSessionId);
       });
 
       test('>getting session as Map', () async {
         final result = await tmdb.v3.auth.createSessionFromV4AccessToken(
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE1ODQzNjgxODYsInN1YiI6IjVkZjEyMTYzZGI5NTJkMDAxOWJlZjAyNiIsImp0aSI6IjE5MDcxMDgiLCJhdWQiOiJjMjVkZDA2ODZmZDEyODk4YzJiMTg2ZTY5ZTY3MjhmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCIsImFwaV93cml0ZSJdLCJ2ZXJzaW9uIjoxfQ.cCgOIDlLk7F0LS-OlMJNl059TucIspxhhENuTW_dE1c',
-            asMap: true) as Map;
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE1ODQzNjgxODYsInN1YiI6IjVkZjEyMTYzZGI5NTJkMDAxOWJlZjAyNiIsImp0aSI6IjE5MDcxMDgiLCJhdWQiOiJjMjVkZDA2ODZmZDEyODk4YzJiMTg2ZTY5ZTY3MjhmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCIsImFwaV93cml0ZSJdLCJ2ZXJzaW9uIjoxfQ.cCgOIDlLk7F0LS-OlMJNl059TucIspxhhENuTW_dE1c',
+          asMap: true,
+        ) as Map;
+        expect(result, isNotNull);
+        expect(result, isA<Map>());
       });
     });
   });
